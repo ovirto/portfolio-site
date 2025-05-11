18141097002dfd0aed4dec249dc73bfcb3884a4f
Ozie Virto <ovirto@outlook.com>
2025-05-10 22:17:57 -0500

    Index.tsx functions

---
+++ portfolio-site/frontend/pages/index.tsx
@@ -0,0 +1,57 @@
+
+
+export default function HeroSection(){
+    // TODO
+    return (
+        <div>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+
+export function AboutMe(){
+    // TODO
+    return (
+        <div>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+
+export function FeaturedProjects(){
+    // TODO
+    return (
+        <div>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+
+export function Skills(){
+    // TODO
+    return (
+        <div>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+
+export function Technologies(){
+    // TODO
+    return (
+        <div>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+

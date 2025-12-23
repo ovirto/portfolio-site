ccd3c557bb73a1fe4bda20b9b0a82d62aa0652b1
Ozie Virto <ovirto@outlook.com>
2025-12-23 11:20:15 -0600

    Wurlde page

--- portfolio-site/frontend/pages/index.tsx
+++ portfolio-site/frontend/pages/index.tsx
@@ -186,7 +186,7 @@ export function Connect(){
                     working on.
                 </p>
                 <a
-                    href=""
+                    href="" // TODO
                     className="inline-block px-4 py-2 border-2 border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-600 hover:text-white transition"
                 >
                     Send me an Email
